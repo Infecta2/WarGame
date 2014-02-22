59b91e1433d4845f22f607c95d06b8c2158c9014
Ivancho <makiavelo2000n@hotmail.com>
2014-02-22 09:21:12 -0500

    mi primer archivo desde java

---
+++ src/Com/GameSoft/WarGame/Front/Prueba.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Com.GameSoft.WarGame.Front;
+
+/**
+ *
+ * @author Ivancho
+ */
+public class Prueba {
+    
+}
